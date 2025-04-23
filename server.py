a85ecd33fb74b32562cca9ba925853f2f728d636
Jennifer Sophia Lopez <57370380+jenlopez1411@users.noreply.github.com>
2025-04-23 15:05:48 -0700

    Update server.py
    Owner wants to recieve customer's message through his personal email instead of using telegram

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ from email.mime.text import MIMEText
 app = Flask(__name__)
 
 
-# BOT_TOKEN = "7621892336:AAHsJrltQ_XGJdJhbneXR6MDlrNbgEitxbk"
-# CHAT_ID = "7829040192"
+# BOT_TOKEN = ""
+# CHAT_ID = ""
 # Your email settings
 EMAIL_ADDRESS = "" # enter gmail
 EMAIL_PASSWORD = "" # Use app password, not your Gmail login, look up gmail settings
